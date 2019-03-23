999d03294cf86f16f2db35c6516b9d457365fd3f
DyakivYurii <dyakivyurii@gmail.com>
2019-03-23 19:01:45 +0200

    more markup

---
+++ src/pages/Auth/SignUp/containers/SignUp.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import SignUpForm from '../components/SignUpForm';
+
+const SignUp = () => {
+  return (
+    <section className="sign-up">
+      <h2 className="sign-up">Sign Up</h2>
+      <SignUpForm />
+    </section>
+  );
+};
+
+export default SignUp;
