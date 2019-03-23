25f7d761ba2f1ed07c5ac26c399b4fedd7a4b4b9
DyakivYurii <dyakivyurii@gmail.com>
2019-03-23 13:05:06 +0200

    Initial structure

---
+++ README.md
@@ -0,0 +1,1 @@
+This project was build for people who want to find a job or an executor of a posted job
---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { BrowserRouter, Route, Redirect, Switch } from 'react-router-dom';
+
+import './App.css';
+
+const App = () => {
+  return <div className="App" />;
+};
+
+export default App;
