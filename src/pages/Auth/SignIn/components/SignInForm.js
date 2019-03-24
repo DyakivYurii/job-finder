bccd7a9f8218f11a72375b3c0a9ff1c7f175564a
DyakivYurii <dyakivyurii@gmail.com>
2019-03-24 12:16:13 +0200

    Some style changes

--- src/pages/Auth/SignIn/components/SignInForm.js
+++ src/pages/Auth/SignIn/components/SignInForm.js
@@ -5,12 +5,18 @@ import './SignInForm.css';
 const SignInForm = () => {
   return (
     <form className="sign-in__form">
-      <input type="email" placeholder="E-mail" className="sign-in__input" />
-      <input
-        type="password"
-        placeholder="Password"
-        className="sign-in__input"
-      />
+      <label>
+        <p>Email:</p>
+        <input type="email" placeholder="E-mail" className="sign-in__input" />
+      </label>
+      <label>
+        <p>Password:</p>
+        <input
+          type="password"
+          placeholder="Password"
+          className="sign-in__input"
+        />
+      </label>
       <button type="submit" className="sign-in__button">
         Sign In
       </button>
--- src/pages/Auth/SignIn/containers/SignIn.js
+++ src/pages/Auth/SignIn/containers/SignIn.js
@@ -2,11 +2,21 @@ import React from 'react';
 
 import SignInForm from '../components/SignInForm';
 
+import './SignIn.css';
+
 const SignIn = () => {
   return (
-    <section className="sign-in">
-      <h2 className="sign-in__title">Sign In</h2>
-      <SignInForm />
+    <section className="sign-in container">
+      <div className="row">
+        <div className="col">
+          <h2 className="sign-in__title">Sign In</h2>
+          <SignInForm className="col" />
+          <p className="sign-in__sign-up-text">
+            Doesn't have an account?{' '}
+            <span className="sign-in__sign-up-text--link">Sign Up</span>
+          </p>
+        </div>
+      </div>
     </section>
   );
 };
