0642587885e0966b9de5938d8df404885fc4757f
DyakivYurii <dyakivyurii@gmail.com>
2019-03-31 10:53:02 +0300

    Configured testing environment. Deleted Bootstrap

--- src/pages/Auth/SignIn/containers/SignIn.js
+++ src/pages/Auth/SignIn/containers/SignIn.js
@@ -7,16 +7,12 @@ import './SignIn.css';
 const SignIn = () => {
   return (
     <section className="sign-in container">
-      <div className="row">
-        <div className="col">
-          <h2 className="sign-in__title">Sign In</h2>
-          <SignInForm className="col" />
-          <p className="sign-in__sign-up-text">
-            Doesn't have an account?{' '}
-            <span className="sign-in__sign-up-text--link">Sign Up</span>
-          </p>
-        </div>
-      </div>
+      <h2 className="sign-in__title">Sign In</h2>
+      <SignInForm />
+      <p className="sign-in__sign-up-text">
+        Doesn't have an account?{' '}
+        <span className="sign-in__sign-up-text--link">Sign Up</span>
+      </p>
     </section>
   );
 };
